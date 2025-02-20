8c6ccd483488f4e4b30b725f5df07e8bd91d1872
WaliaRohan <walia.rohan@gmail.com>
2025-02-20 11:55:32 -0500

    Implementing hb_dot for belief CBF

--- cbf_lite/cbfs.py
+++ cbf_lite/cbfs.py
@@ -1,5 +1,7 @@
 import jax.numpy as jnp
 from jax.scipy.special import erfinv
+from jax import random, jacfwd, jacrev 
+import jax
 
 # CLF: V(x) = ||x - goal||^2
 def vanilla_clf(x, goal):
@@ -17,6 +19,10 @@ def vanilla_cbf_wall(x, wall_x):
 import numpy as np
 
 
+KEY = random.PRNGKey(0)
+KEY, SUBKEY = random.split(KEY)
+
+
 def belief_cbf_half_space(alpha, beta, delta, mu, sigma):
     '''
     alpha: linear gain from half-space constraint (alpha^T.x >= B, where x is the state)
@@ -30,23 +36,43 @@ def belief_cbf_half_space(alpha, beta, delta, mu, sigma):
     term2 = jnp.sqrt(2 * jnp.dot(alpha.T, jnp.dot(sigma, alpha))) * erfinv(1 - 2 * delta)
     return term1 - term2
 
+# def get_diff(function, x, sigma):
 
-def h_b_rb2(alpha, beta, mu, sigma, delta):
-    '''
-    Function for calculating barrier function for h_b where relative degree is 2
-    
-    '''
+#     subkey = SUBKEY # globally defined
 
-    roots = jnp.array([-0.1]) # Manually select root to be in left half plane
-    polynomial = np.poly1d(roots, r=True)
-    coeff = jnp.array(polynomial.coeffs)
+#     n_samples = 10
+#     state_dim = len(x)
 
-    h_0 = h_b(alpha, beta, mu, sigma, delta)
-    h_1 = jnp.grad(h_b, argnums=2) # 1st order derivative, take derivative with respect to mean of belief
+#     samples = random.normal(subkey, (n_samples, state_dim))
+#     jacobian = jacfwd(function)
 
-    h_2 = jnp.array(h_0*coeff[0]) + jnp.array(h_1*coeff[1]) # equation 4 (belief paper), equation 38 (ECBF paper)
+#     total = 0
+#     for sample in samples:
+#         _, dyn_g = system_dynamics(sample[:-1])
+#         grad = jacobian(sample)[:-1]
+#         total += jnp.sum(jnp.abs(jnp.matmul(grad, dyn_g)))
 
-    return h_2
+#     def exponential_new_func(x: Array):
+#         return jnp.matmul(jacobian(x)[:-1], system_dynamics(x[:-1])[0])
+
+
+# def h_b_rb2(alpha, beta, mu, sigma, delta):
+#     '''
+#     Function for calculating barrier function for h_b where relative degree is 2
+#     '''
+
+#     roots = jnp.array([-0.1]) # Manually select root to be in left half plane
+#     polynomial = np.poly1d(roots, r=True)
+#     coeff = jnp.array(polynomial.coeffs)
+
+#     h_0 = belief_cbf_half_space(alpha, beta, mu, sigma, delta)
+#     h_1 = jnp.grad(h_0, argnums=2) # 1st order derivative, take derivative with respect to mean of belief
+
+#     h_2 = jnp.array(h_0*coeff[0]) + jnp.array(h_1*coeff[1]) # equation 4 (belief paper), equation 38 (ECBF paper)
+
+#     return h_2
+
+
 
 
 
--- cbf_lite/test.py
+++ cbf_lite/test.py
@@ -1,18 +1,47 @@
 from functools import partial
 import jax.numpy as jnp
 from cbfs import belief_cbf_half_space as h_b
+# from cbfs import b_dot
+from dynamics import *
 
-# Define fixed parameters
-alpha = jnp.eye(4)  # Example 4x4 matrix
-beta = jnp.ones((4, 1))  # Example 4x1 vector
-delta = 0.05  # Probability threshold
-# Partially apply h_b with fixed alpha, beta, and sigma
-h_b_partial = partial(h_b, alpha, beta, delta)
+import jax
+# Multi-dimensional
+# mu = jnp.array([[0.5], [0.2], [-0.3], [0.1]])  # Example 4x1 mean vector
+# sigma = jnp.eye(4) * 0.1  # Example 4x4 covariance matrix
+# alpha = jnp.eye(4)  # Example 4x4 matrix
+# beta = jnp.ones((4, 1))  # Example 4x1 vector
+# delta = 0.05  # Probability threshold
 
-# Example usage
-mu = jnp.array([[0.5], [0.2], [-0.3], [0.1]])  # Example 4x1 mean vector
+# Single dimension
+# mu = 0.5
+# sigma = 0.1
+# alpha = jnp.array([1.0])  # Example 4x4 matrix
+# beta = 10.0 # Example 4x1 vector
+# delta = 0.05  # Probability threshold
 
-sigma = jnp.eye(4) * 0.1  # Example 4x4 covariance matrix
+# # Partially apply h_b with fixed alpha, beta, and sigma
+# h_b_partial = partial(h_b, alpha, beta, delta)
 
-result = h_b_partial(mu, sigma)
-print(result)
+# result = h_b_partial(mu, sigma)
+# print(result)
+
+dynamics = SimpleDynamics()
+
+dt = dt = 0.1
+# Q = jnp.eye(len(dynamics.A)) * 0.01
+
+mu = jnp.ones(2) * 0.5
+sigma = jnp.eye(2) * 0.1
+
+jac_f_x = jax.jacfwd(dynamics.f, argnums=0)(mu) # jacobian of f evaluated at mu
+jac_g_x = jax.jacfwd(dynamics.g, argnums=0)(mu) # jacobian of g evaluated at mu
+
+jac_f_x_sigma = jac_f_x@sigma + sigma@jac_f_x.T + dynamics.Q/2
+jac_g_x_sigma = jac_g_x@sigma + sigma@jac_g_x.T + dynamics.Q/2
+
+
+
+# mu_dot, sigma_dot = b_dot(mu, sigma, dynamics, Q, dt)
+
+# beta = jnp.ones(len(dynamics.A))
+# delta = 0.05
